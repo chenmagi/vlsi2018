e2ca47e1c2fca8484692c74f87ef9b8cba597132
Chen Yung chih <chenmagi@gmail.com>
2018-12-03 12:52:13 +0800

    extend searching range

--- HW3/src/run_seedgen.sh
+++ HW3/src/run_seedgen.sh
@@ -3,11 +3,13 @@ mkdir -p .seedgen
 export hw3_time_limit=1
 if [ $# -ge 2 ]; then ratio=$2; else ratio=0.1; fi
 mkdir -p .seedgen/n$1
-for i in `seq 1 100`; do 
+for i in `seq 1 200`; do 
 rand=$RANDOM
 export hw3_seed=$rand
-echo "Iteration "$i ", rand="$rand
+echo -n "Iteration "$i ", rand="$rand "-->"
 ./seed_gen ../testcase/n$1.hardblocks ../testcase/n$1.nets ../testcase/n$1.pl .log/n$1.floorplan $ratio > .seedgen/n$1/r$rand.log
+line=`grep ela ./.seedgen/n$1/r$rand.log`
+echo $line
 done
 unset hw3_time_limit
 
